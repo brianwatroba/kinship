b71ce5e84362ad9c2306e6abd384565e75bf08c2
brian watroba <brianwatroba@gmail.com>
2023-07-11 17:03:21 -0400

    sendSms units finished

--- __tests__/unit/api/sms.test.ts
+++ __tests__/unit/api/sms.test.ts
@@ -1,7 +1,30 @@
+import { sendSms } from "../../../app/api/sms/_utils";
+const TEST_NUMBERS = {
+  VALID: "+15005550006",
+  INVALID: "+15005550001",
+};
 describe("api/sms/_utils", () => {
   describe("sendSms()", () => {
-    it("Success: should return true", async () => {
-      expect(true).toBe(true);
+    it("Success: sends successfully (text)", async () => {
+      const to = TEST_NUMBERS.VALID;
+      const from = process.env.TWILIO_PHONE_NUMBER as string;
+      const params = { to, from, body: "test" };
+      const res = await sendSms(params);
+      expect(res).toBe(true);
+    });
+    it("Success: sends successfully (media)", async () => {
+      const to = TEST_NUMBERS.VALID;
+      const from = process.env.TWILIO_PHONE_NUMBER as string;
+      const mediaUrls = ["https://api.api-ninjas.com/v1/randomimage?category=nature"];
+      const params = { to, from, mediaUrls };
+      const res = await sendSms(params);
+      expect(res).toBe(true);
+    });
+    it("Fails: invalid to number", async () => {
+      const to = TEST_NUMBERS.INVALID;
+      const from = process.env.TWILIO_PHONE_NUMBER as string;
+      const params = { to, from, body: "test" };
+      await expect(sendSms(params)).rejects.toThrow();
     });
   });
 });
--- app/api/sms/_utils.ts
+++ app/api/sms/_utils.ts
@@ -4,8 +4,9 @@ type sendSmsParams = { to: string; from: string; body?: string; mediaUrls?: stri
 
 export const sendSms = async (params: sendSmsParams) => {
   const twilioClient = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-  const { body, to, from, mediaUrls } = params;
-  const { status } = await twilioClient.messages.create({ body, to, from, mediaUrl: mediaUrls });
+  const { to, from, body, mediaUrls } = params;
+  const { status } = await twilioClient.messages.create({ to, from, body, mediaUrl: mediaUrls });
   const sentSuccess = ["queued", "sending", "sent"].includes(status);
   if (!sentSuccess) throw new Error(`Failed to send twilio text. Status: ${status}, params: ${JSON.stringify(params)}`);
+  return sentSuccess;
 };
