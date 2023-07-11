087bbe635cc8745aa462f123d4894d02ce017cdd
brian watroba <brianwatroba@gmail.com>
2023-07-11 15:04:53 -0400

    fixed broken receive sms route

--- app/api/sms/send/route.ts
+++ app/api/sms/send/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-export function POST(request: NextRequest) {
+export function GET(request: NextRequest) {
   return NextResponse.json(
     {
       body: request.body,
