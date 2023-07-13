e1b7477556bb46bca834d021a31cc3bd323f04d9
brian watroba <brianwatroba@gmail.com>
2023-07-13 15:40:57 -0400

    add other og data

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,19 @@ import "./globals.css";
 export const metadata = {
   title: "Kinship",
   description: "SMS-based daily question game that helps families feel more connected.",
+  openGraph: {
+    title: "Kinship",
+    description: "SMS-based daily question game that helps families feel more connected.",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dfuyisjqi/image/upload/v1689265310/kinship/Logos/kinshipnavbar_ygxchv.png",
+        width: 300,
+        height: 88,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
